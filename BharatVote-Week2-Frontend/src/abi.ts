abb719f900cb840b1cbf44206d20f338d2d564a8
Archee Arjun <archeearjun@gmail.com>
2026-01-02 04:05:43 +0530

    Remove weekly snapshot folders

--- BharatVote-Week2-Frontend/src/abi.ts
+++
@@ -1,8 +0,0 @@
-// src/abi.ts
-
-// Vite + TS can import JSON directly if "resolveJsonModule": true
-// in tsconfig.json (usually already set in Vite templates)
-import BharatVoteJson from "./contracts/BharatVote.json";
-
-export const contractABI = BharatVoteJson.abi;
-export const contractAddress = BharatVoteJson.address as string;
